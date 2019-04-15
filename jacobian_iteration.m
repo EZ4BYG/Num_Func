80341bfa9aa7ed8d6a814fe1cc0070325457a6ed
GaoBoYu599 <44675030+GaoBoYu599@users.noreply.github.com>
2019-04-15 09:55:46 +0800

    (未预处理)线性方程组迭代法

---
+++ jacobian_iteration.m
@@ -0,0 +1,42 @@
+% 线性方程组近似求解: 雅克比迭代"近似"求解线性方程组
+% 说明1: 雅克比迭代矩阵B1是判断收敛与否的关键! B1的谱半径(B1的绝对值最大的特征值)要小于1。
+% 说明2: 谱半径越小(<1)收敛速度越快!  
+
+clear; clc;
+
+% 关于原方程组的迭代计算必备矩阵: 不同方程每次修改A和b即可。
+A = [10 -1 2 0;-1 11 -1 3;2 -1 10 -1;0 3 -1 8];  % 系数矩阵
+b = [7;14;-10;12];        % 等号右边结果(列矩阵)
+D = diag(diag(A));
+E = eye(length(b));
+B1 = E-inv(D)*A;    % 雅克比迭代矩阵, 用来计算和判断收敛与否
+
+if det(A) == 0
+    fprintf('系数矩阵A为奇异矩阵,无法求解!\n');
+    return ;
+end
+
+radius = max(abs(eig(B1)));
+if radius > 1
+    fprintf('当前迭代不收敛!\n');
+    return ;
+else
+    fprintf('雅克比迭代矩阵谱半径为: %.4f\n',radius);
+end
+    
+% 迭代计算部分:
+x = zeros(length(b),1);  % 初始迭代4x1的矩阵(列矩阵)
+error = double(input('输入迭代精度(例如10^(-4)或0.0001):'));
+count = 0;    % 迭代计数器
+while 1
+    tmp = B1*x + inv(D)*b;
+    if max(abs(tmp - x)) < error
+        break;
+    end
+    x = tmp;
+    count = count + 1;
+end
+
+fprintf('方程的解为: x1 = %.4f  x2 = %.4f  x3 = %.4f  x4 = %.4f\n',x(1),x(2),x(3),x(4));
+fprintf('共迭代%d次\n',count);
+
---
+++ relaxation_iteration.m
@@ -0,0 +1,44 @@
+% 线性方程组近似求解: 松弛迭代
+% 思路: 在赛德尔迭代的基础上, 再加一个松弛因子w来调控收敛速度;
+% 说明: 松弛迭代可能加速也可能降速! 并且可能原来赛德尔收敛, 但是加了w后就发散了。
+% 注意: w一般在[0,2]之间, w=1就是赛德尔迭代, w>1一般加速, 0<w<1一般减速。
+
+clear; clc;
+
+% 关于原方程组的迭代计算必备矩阵: 不同方程每次修改A和b即可。
+A = [10 -1 2 0;-1 11 -1 3;2 -1 10 -1;0 3 -1 8];  % 系数矩阵
+b = [7;14;-10;12];          % 等号右边结果(列矩阵)
+D = diag(diag(A));
+L = tril(A,-1);    % 向左下移一格的下三角阵;
+U = triu(A,1);     % 向右上移一格的上三角阵;
+w = double(input('输入松弛因子w(一般0~2):'));
+B3 = inv(D+w*L) * ( (1-w)*D - w*U );   % (超)松弛迭代矩阵, 用来计算和判断收敛与否;
+
+if det(A) == 0
+    fprintf('系数矩阵A为奇异矩阵,无法求解!\n');
+    return ;
+end
+
+radius = max(abs(eig(B3)));  % 特征值有可能为复数, abs取绝对值 + 取模
+if radius > 1
+    fprintf('当前迭代不收敛!\n');
+    return ;
+else
+    fprintf('松弛迭代矩阵谱半径为: %.4f\n',radius);
+end
+    
+% 迭代计算部分:
+x = zeros(length(b),1);  % 初始迭代4x1的矩阵(列矩阵)
+error = double(input('输入迭代精度(例如10^(-4)或0.0001):'));
+count = 0;    % 迭代计数器
+while 1
+    tmp = B3*x + w*inv(D+w*L)*b;
+    if max(abs(tmp - x)) < error
+        break;
+    end
+    x = tmp;
+    count = count + 1;
+end
+
+fprintf('方程的解为: x1 = %.4f  x2 = %.4f  x3 = %.4f  x4 = %.4f\n',x(1),x(2),x(3),x(4));  % 这里记得根据方程修改个数
+fprintf('共迭代%d次\n',count);
